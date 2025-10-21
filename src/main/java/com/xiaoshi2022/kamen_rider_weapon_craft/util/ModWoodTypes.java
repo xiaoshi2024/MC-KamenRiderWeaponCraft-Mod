7a63423af04d6f82263300f5e4b269bd542adccd
xiaoshi2024 <2537530079@qq.com>
2025-10-21 16:13:57 +0800

    Delete src directory

--- src/main/java/com/xiaoshi2022/kamen_rider_weapon_craft/util/ModWoodTypes.java
+++
@@ -1,9 +0,0 @@
-//package com.xiaoshi2022.kamen_rider_weapon_craft.util;
-//
-//import com.xiaoshi2022.kamen_rider_weapon_craft.kamen_rider_weapon_craft;
-//import net.minecraft.world.level.block.state.properties.BlockSetType;
-//import net.minecraft.world.level.block.state.properties.WoodType;
-//
-//public class ModWoodTypes {
-//    public static final WoodType PINE = WoodType.register(new WoodType(kamen_rider_weapon_craft.MOD_ID + ":pine", BlockSetType.OAK));
-//}
