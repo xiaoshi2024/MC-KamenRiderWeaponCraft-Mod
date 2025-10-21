4d2cea1afa12fad8b8eb8c7836648a76f1017dcf
xiaoshi2024 <2537530079@qq.com>
2025-10-21 17:17:12 +0800

    1.21.6fabric

---
+++ src/main/java/com/xiaoshi2022/kamen_rider_weapon_craft/KRWClinet.java
@@ -0,0 +1,10 @@
+package com.xiaoshi2022.kamen_rider_weapon_craft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KRWClinet implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
