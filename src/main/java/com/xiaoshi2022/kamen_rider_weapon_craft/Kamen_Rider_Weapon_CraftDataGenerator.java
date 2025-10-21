579ee4c434ffcd64b5da54eabe08f240c07bad2d
xiaoshi2024 <2537530079@qq.com>
2025-10-21 16:59:50 +0800

    修正1.21.6fabric

---
+++ src/main/java/com/xiaoshi2022/kamen_rider_weapon_craft/Kamen_Rider_Weapon_CraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.xiaoshi2022.kamen_rider_weapon_craft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Kamen_Rider_Weapon_CraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
--- src/main/java/com/xiaoshi2022/kamen_rider_weapon_craft/kamen_rider_weapon_craft.java
+++ src/main/java/com/xiaoshi2022/kamen_rider_weapon_craft/kamen_rider_weapon_craft.java
@@ -1,196 +1,24 @@
 package com.xiaoshi2022.kamen_rider_weapon_craft;
 
-import com.xiaoshi2022.kamen_rider_weapon_craft.event.ItemTooltipHandler;
-import com.xiaoshi2022.kamen_rider_weapon_craft.event.LivingHurtHandler;
-import com.xiaoshi2022.kamen_rider_weapon_craft.event.WitherSpawnHandler;
-import com.xiaoshi2022.kamen_rider_weapon_craft.network.*;
-import com.xiaoshi2022.kamen_rider_weapon_craft.particle.ModParticles;
-import com.xiaoshi2022.kamen_rider_weapon_craft.procedures.KRWBoot;
-import com.xiaoshi2022.kamen_rider_weapon_craft.procedures.PullSounds;
-import com.xiaoshi2022.kamen_rider_weapon_craft.procedures.PullSoundsClient;
-import com.xiaoshi2022.kamen_rider_weapon_craft.recipe.ModRecipes;
-import com.xiaoshi2022.kamen_rider_weapon_craft.registry.*;
-import com.xiaoshi2022.kamen_rider_weapon_craft.rider.RiderEffectEntityRegistry;
-import com.xiaoshi2022.kamen_rider_weapon_craft.tab.ModTab;
-import com.xiaoshi2022.kamen_rider_weapon_craft.util.FruitConversionRegistry;
-import com.xiaoshi2022.kamen_rider_weapon_craft.villagers.LockSeedMerchantProfession;
-import com.xiaoshi2022.kamen_rider_weapon_craft.villagers.TimeTravelerProfession;
-import com.xiaoshi2022.kamen_rider_weapon_craft.worldgen.tree.ModFoliagePlacers;
-import com.xiaoshi2022.kamen_rider_weapon_craft.worldgen.tree.ModTrunkPlacerTypes;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.ComposterBlock;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.util.thread.SidedThreadGroups;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.MixinEnvironment;
-import software.bernie.geckolib.GeckoLib;
+import net.fabricmc.api.ModInitializer;
 
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Kamen_Rider_Weapon_Craft implements ModInitializer {
+	public static final String MOD_ID = "kamen_rider_weapon_craft";
 
-@Mod("kamen_rider_weapon_craft")
-@Mod.EventBusSubscriber(modid = "kamen_rider_weapon_craft")
-public class kamen_rider_weapon_craft {
-    public static final String MOD_ID = "kamen_rider_weapon_craft";
-    private static final String PROTOCOL_VERSION = "1";
-    public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation(MOD_ID, MOD_ID),
-            () -> PROTOCOL_VERSION,
-            PROTOCOL_VERSION::equals,
-            PROTOCOL_VERSION::equals
-    );
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public kamen_rider_weapon_craft() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
 
-        ModSounds.REGISTRY.register(modEventBus);
-        ModItems.ITEMS.register(modEventBus);
-        ModBlocks.BLOCKS.register(modEventBus);
-        ModFluids.FLUIDS.register(modEventBus);
-        ModFluids.FLUID_TYPES.register(modEventBus);
-        ModTab.TABS.register(modEventBus);
-        ModBlockEntities.BLOCK_ENTITIES.register(modEventBus);
-        ModParticles.REGISTRY.register(modEventBus);
-        MinecraftForge.EVENT_BUS.register(kamen_rider_weapon_craft.class);
-        GeckoLib.initialize();
-
-        MinecraftForge.EVENT_BUS.register(LivingHurtHandler.class);
-
-        MinecraftForge.EVENT_BUS.register(WitherSpawnHandler.class);
-        
-        // 注册物品提示处理器
-        MinecraftForge.EVENT_BUS.register(ItemTooltipHandler.class);
-        
-        // 注册武器提示处理器 - 为所有ModItems中注册的武器添加统一提示
-//        MinecraftForge.EVENT_BUS.register(WeaponTooltipHandler.class);
-
-        FruitConversionRegistry.init();
-
-   /// 注册配方
-        ModRecipes.RECIPE_TYPES.register(modEventBus);
-        ModRecipes.RECIPE_SERIALIZERS.register(modEventBus);
-
-        MinecraftForge.EVENT_BUS.register(PullSounds.class);
-        MinecraftForge.EVENT_BUS.register(KRWBoot.class);
-        MinecraftForge.EVENT_BUS.register(PullSoundsClient.class);
-
-        //树苗的注册
-        ModFoliagePlacers.register(modEventBus);
-        ModTrunkPlacerTypes.register(modEventBus);
-
-        // 自定义村民职业
-        TimeTravelerProfession.POI_TYPE.register(modEventBus);
-        TimeTravelerProfession.PROFESSION.register(modEventBus);
-
-        LockSeedMerchantProfession.POI_TYPE.register(modEventBus);
-        LockSeedMerchantProfession.PROFESSION.register(modEventBus);
-
-        // 初始化容器
-        ModContainers.REGISTRY.register(modEventBus);
-
-        // 初始化 MODEntity
-        ModEntityTypes.ENTITIES.register(modEventBus);
-        
-        // 注册骑士效果实体
-        RiderEffectEntityRegistry.register(modEventBus);
-
-        // 初始化 Mixin 系统
-        MixinBootstrap.init();
-        MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
-        MixinEnvironment.getEnvironment(MixinEnvironment.Phase.DEFAULT);
-
-        // 注册网络包
-        registerNetworkMessages();
-
-        // 确保这行存在！这是关键！
-        modEventBus.addListener(this::commonSetup);
-
-        // 注册 NetworkHandler
-        NetworkHandler.register();
-
-        EffectInit.EFFECTS.register(modEventBus);
-    }
-
-    @SubscribeEvent
-    public void commonSetup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(() -> {
-            // 确保在主线程中操作
-            Item leavesItem = ModItems.PINE_LEAVES_ITEM.get();
-
-            // 直接操作 public 的 COMPOSTABLES map
-            ComposterBlock.COMPOSTABLES.put(leavesItem, 0.3f);
-
-            ComposterBlock.COMPOSTABLES.put(ModItems.PINE_SAPLING_ITEM.get(), 0.3f);
-
-        });
-    }
-
-
-
-    private void registerNetworkMessages() {
-        int id = 0;
-        PACKET_HANDLER.registerMessage(id++, LockseedManager.class, LockseedManager::buffer, LockseedManager::new, LockseedManager::handler);
-        PACKET_HANDLER.registerMessage(
-                id++,
-                CloseMapPacket.class,
-                CloseMapPacket::encode,
-                CloseMapPacket::decode,
-                CloseMapPacket::handle
-        );
-        // 新增水果转换包注册
-        PACKET_HANDLER.registerMessage(
-                id++,
-                FruitConversionPacket.class,
-                FruitConversionPacket::encode,
-                FruitConversionPacket::decode,
-                FruitConversionPacket::handle
-        );
-        // 注册 SeverSound 数据包
-        PACKET_HANDLER.registerMessage(
-                id++,
-                ServerSound.class,
-                ServerSound::encode,
-                ServerSound::decode,
-                ServerSound::handle
-        );
-    }
-
-    // 任务队列
-    private static final Collection<AbstractMap.SimpleEntry<Runnable, Integer>> workQueue = new ConcurrentLinkedQueue<>();
-
-    public static void queueServerWork(int tick, Runnable action) {
-        if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER) {
-            workQueue.add(new AbstractMap.SimpleEntry<>(action, tick));
-        }
-    }
-
-    @SubscribeEvent
-    public static void tick(TickEvent.ServerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            // 处理任务队列
-            List<AbstractMap.SimpleEntry<Runnable, Integer>> actions = new ArrayList<>();
-            workQueue.forEach(work -> {
-                work.setValue(work.getValue() - 1);
-                if (work.getValue() == 0) {
-                    actions.add(work);
-                }
-            });
-            actions.forEach(e -> e.getKey().run());
-            workQueue.removeAll(actions);
-
-        }
-    }
+		LOGGER.info("Hello Fabric world!");
+	}
 }
